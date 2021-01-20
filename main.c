65fd3e467ed9d25446ad39e81831bfe3b8561ffa
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-01-20 13:00:42 +0330

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+   char statement = '';
+   if(scanf("%c",&statement) == 'e'){
+      printf("\n The file that you want to open it opned it writen in that it");
+   }
+}
