fe30b1e03c5e3d185c6d0d25a330c64e07a2ebe9
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-01-20 13:07:41 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(){
-   char statement = '';
-   if(scanf("%c",&statement) == 'e'){
-      printf("\n The file that you want to open it opned it writen in that it");
+   char statement = 'a';
+   scanf("%c",&statement);
+   if(statement == 'e'){
+      printf("\nThe file that you want to open it opned it writen in that it");
+   }else{
+        printf("your statement is not executable");
    }
+   return 0;
 }
+
