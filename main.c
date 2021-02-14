f814efaf6c98a4777eb15e2aad4d6f0d38219690
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-14 14:37:35 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    char statement = 'a';
-    scanf("%c",&statement);
-    if(statement == 'e'){
+    char statement[];
+    scanf("%s",&statement);
+    if(statement == "){
             printf("Hello , World!");
     }else {
         printf("Your command does not exits");
